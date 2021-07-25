f63089fb7c4d641849796ed3ebedc1afc0d3eb4c
Black devil <79362335+devan1234567@users.noreply.github.com>
2021-07-25 10:45:41 +0530

    Add files via upload

---
+++ root.sh
@@ -0,0 +1,10 @@
+toilet -f future KHC -F gay
+echo "          Created by BLACK_DEVIL"
+
+toilet -f future HINT
+echo "(1)exif"
+echo "(2)decode base664"
+echo "(3)tricky challenge"
+
+
+toilet -f future THANK YOU -F gay
